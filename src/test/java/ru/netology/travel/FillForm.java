78e6efeb17def8913e5c69f3922a7ffdd3b27665
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-10-15 13:42:59 +0300

    Update FillForm.java

--- src/test/java/ru/netology/travel/FillForm.java
+++ src/test/java/ru/netology/travel/FillForm.java
@@ -14,35 +14,72 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FillForm {
-
+ private SelenideElement heading = $(withText("Кредит по данным карты"));
+    private SelenideElement cardNumber = $(withText("Номер карты")).parent().$(".input__control");
+    private SelenideElement month = $(withText("Месяц")).parent().$(".input__control");
+    private SelenideElement year = $(withText("Год")).parent().$(".input__control");
+    private SelenideElement owner = $(withText("Владелец")).parent().$(".input__control");
+    private SelenideElement codeCVV = $(withText("CVC/CVV")).parent().$(".input__control");
+    private SelenideElement continueButton = $$("button").find(exactText("Продолжить"));
+    private SelenideElement fieldError = $(".input_invalid .input__sub");
+    private SelenideElement fieldErrorForCVV = $(withText("CVC/CVV")).parent().$(".input__sub");
+    private SelenideElement errorMessage = $(".notification_status_error");
+    private SelenideElement successMessage = $(".notification_status_ok");
+    private SelenideElement closeMessage = $(".notification__closer");
+    
 
     public void fillCorrectForm () {
-        $$("[class=input__control]").get(3).setValue("Anna");
-        $("[class=input__box] [placeholder='22']").setValue("23");
-        $("[class=input__box] [placeholder='0000 0000 0000 0000']").setValue("4444 4444 4444 4441");
-        $("[class=input__box] [placeholder='08']").setValue("10");
-        $("[class=input__box] [placeholder='999']").setValue("123");
-        $(byText("Продолжить")).click();
+        cardNumber.setValue(card.getCardNumber());
+        month.setValue(card.getMonth());
+        year.setValue(card.getYear());
+        owner.setValue(card.getOwner());
+        codeCVV.setValue(card.getCodeCVV());
+        continueButton.click();
     }
 
-    public void fillIncorrectCard () {
-        $$("[class=input__control]").get(3).setValue("Anna");
-        $("[class=input__box] [placeholder='22']").setValue("23");
-        $("[class=input__box] [placeholder='0000 0000 0000 0000']").setValue("4444 4444 4444 4442");
-        $("[class=input__box] [placeholder='08']").setValue("10");
-        $("[class=input__box] [placeholder='999']").setValue("123");
-        $(byText("Продолжить")).click();
+     public void shouldGiveFieldErrorWhenIncorrectCardExpirationDate() {
+        fieldError.shouldHave(Condition.text("Неверно указан срок действия карты"))
+        .shouldBe(visible, Duration.ofMillis(15000));
+    }
+    
+ public void shouldGiveFieldErrorWhenRequiredField() {
+        fieldError.shouldHave(Condition.text("Поле обязательно для заполнения"))
+         .shouldBe(visible, Duration.ofMillis(15000));
     }
 
-    public void fillIncorrectData(month, year) () {
-        $$("[class=input__control]").get(3).setValue("Anna");
-        $("[class=input__box] [placeholder='22']").setValue("10");
-        $("[class=input__box] [placeholder='0000 0000 0000 0000']").setValue("4444 4444 4444 4441");
-        $("[class=input__box] [placeholder='08']").setValue("90");
-        $("[class=input__box] [placeholder='999']").setValue("123");
-        $(byText("Продолжить")).click();
+    public void shouldGiveFieldErrorForCVVWhenRequiredField() {
+        fieldErrorForCVV.shouldHave(Condition.text("Поле обязательно для заполнения"))
+        .shouldBe(visible, Duration.ofMillis(15000));
     }
 
+    public void shouldGiveFieldErrorWhenValueCannotBeLong() {
+        fieldError.shouldHave(Condition.text("Значение поля не может быть длиннее 70 символов"))
+       .shouldBe(visible, Duration.ofMillis(15000));
+        
+    }
+    
+    public void shouldGiveFieldErrorWhenNameIsNotInLatin() {
+        fieldError.shouldHave(Condition.text("Значение поля может содержать только латинские буквы и дефис"))
+                .shouldBe(visible, Duration.ofMillis(15000));
+    
+    
+    }
+ public void shouldGiveFieldErrorWhenIncorrectCVV() {
+        fieldError.shouldHave(Condition.text("Неверно указан CVC/CVV"))
+                .shouldBe(visible, Duration.ofMillis(15000));
+    }
+
+    public void shouldGiveFieldError() {
+        fieldError.shouldBe(visible, Duration.ofMillis(15000));
+    }
+
+    public void shouldNotGiveFieldError() {
+        fieldError.shouldNotBe(visible, Duration.ofMillis(15000));
+    }
+
+    public void shouldGiveErrorMessage() {
+        errorMessage.shouldBe(visible, Duration.ofMillis(15000));
+    }
 
     public void continueButton(){
         $(byText("Продолжить")).click();
