462d8a13259f38f9f76bf8e439568e9afea95278
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-10-15 14:02:52 +0300

    Update FillForm.java

--- src/test/java/ru/netology/travel/FillForm.java
+++ src/test/java/ru/netology/travel/FillForm.java
@@ -14,29 +14,37 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FillForm {
- private SelenideElement heading = $(withText("Кредит по данным карты"));
-    private SelenideElement cardNumber = $(withText("Номер карты")).parent().$(".input__control");
-    private SelenideElement month = $(withText("Месяц")).parent().$(".input__control");
-    private SelenideElement year = $(withText("Год")).parent().$(".input__control");
-    private SelenideElement owner = $(withText("Владелец")).parent().$(".input__control");
-    private SelenideElement codeCVV = $(withText("CVC/CVV")).parent().$(".input__control");
-    private SelenideElement continueButton = $$("button").find(exactText("Продолжить"));
-    private SelenideElement fieldError = $(".input_invalid .input__sub");
-    private SelenideElement fieldErrorForCVV = $(withText("CVC/CVV")).parent().$(".input__sub");
-    private SelenideElement errorMessage = $(".notification_status_error");
-    private SelenideElement successMessage = $(".notification_status_ok");
-    private SelenideElement closeMessage = $(".notification__closer");
+
+  public void fillCorrectForm () {
+        $$("[class=input__control]").get(3).setValue("Anna");
+        $("[class=input__box] [placeholder='22']").setValue("23");
+        $("[class=input__box] [placeholder='0000 0000 0000 0000']").setValue("4444 4444 4444 4441");
+        $("[class=input__box] [placeholder='08']").setValue("10");
+        $("[class=input__box] [placeholder='999']").setValue("123");
+        $(byText("Продолжить")).click();
+    }
     
 
-    public void fillCorrectForm () {
-        cardNumber.setValue(card.getCardNumber());
-        month.setValue(card.getMonth());
-        year.setValue(card.getYear());
-        owner.setValue(card.getOwner());
-        codeCVV.setValue(card.getCodeCVV());
-        continueButton.click();
+  public void fillIncorrectCard () {
+        $$("[class=input__control]").get(3).setValue("Anna");
+        $("[class=input__box] [placeholder='22']").setValue("23");
+        $("[class=input__box] [placeholder='0000 0000 0000 0000']").setValue("4444 4444 4444 4442");
+        $("[class=input__box] [placeholder='08']").setValue("10");
+        $("[class=input__box] [placeholder='999']").setValue("123");
+        $(byText("Продолжить")).click();
     }
 
+    public void fillIncorrectDataMonthAndYear () {
+        $$("[class=input__control]").get(3).setValue("Anna");
+        $("[class=input__box] [placeholder='22']").setValue("10");
+        $("[class=input__box] [placeholder='0000 0000 0000 0000']").setValue("4444 4444 4444 4441");
+        $("[class=input__box] [placeholder='08']").setValue("90");
+        $("[class=input__box] [placeholder='999']").setValue("123");
+        $(byText("Продолжить")).click();
+    }
+ 
+
+ 
     public void continueButton(){
         $(byText("Продолжить")).click();
     }
